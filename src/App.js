d822af56b9192430804640fa9a028580fdcdf4bf
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-27 10:24:49 +0800

    convert App to  functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import TextField from './components/TextField/TextField';
 import NameInfo from './components/NameInfo/NameInfo';
 import PersonalInfo from './components/PersonalInfo/PersonalInfo';
 import ExperienceList from './components/WorkExperience/ExperienceList';
@@ -8,113 +7,80 @@ import AddExperience from './components/WorkExperience/AddExperience';
 import EducationList from './components/Education/EducationList';
 import AddEducation from './components/Education/AddEducation';
 
-import { Component } from 'react';
+import { useState } from 'react';
 
-class App extends Component {
-    constructor(props) {
-        super();
-        this.state = {
-            experiences: [],
-            education: [],
-            skills: [],
-            preview: false,
-            buttonText: 'Edit Mode',
-        };
+const App = (props) => {
+    const [experiences, setExperiences] = useState([]);
+    const [education, setEducation] = useState([]);
+    const [preview, setPreview] = useState(false);
+    const [buttonText, setButtonText] = useState('Edit Mode');
 
-        this.removeExp = this.removeExp.bind(this);
-        this.removeEdu = this.removeEdu.bind(this);
-
-        this.switchMode = this.switchMode.bind(this);
-    }
-
-    addExperience = (experience) => {
-        this.setState(
-            {
-                experiences: this.state.experiences.concat(experience),
-            },
-            () => {
-                console.log(this.state.experiences);
-            }
-        );
+    const addExperience = (experience) => {
+        setExperiences(experiences.concat(experience));
     };
 
-    addEducation = (education) => {
-        this.setState(
-            {
-                education: this.state.education.concat(education),
-            },
-            () => {
-                console.log(this.state.education);
-            }
-        );
+    const addEducation = (education) => {
+        setEducation(education.concat(education));
     };
 
-    removeExp(id) {
-        const experiences = this.state.experiences.filter(
+    const removeExp = (id) => {
+        const newExperiences = experiences.filter(
             (experience) => experience.id !== id
         );
+        setExperiences(newExperiences);
+    };
 
-        this.setState({ experiences });
-    }
-
-    removeEdu(id) {
-        const education = this.state.education.filter(
+    const removeEdu = (id) => {
+        const newEducation = education.filter(
             (education) => education.id !== id
         );
-        this.setState({ education });
-    }
-    switchMode() {
-        if (this.state.preview) {
-            this.setState({
-                preview: false,
-                buttonText: 'Preview Mode',
-            });
+        setEducation(newEducation);
+    };
+
+    const switchMode = () => {
+        if (preview) {
+            setPreview(false);
+            setButtonText('Preview Mode');
         } else {
-            this.setState({
-                preview: true,
-                buttonText: 'Edit Mode',
-            });
+            setPreview(true);
+            setButtonText('Edit Mode');
         }
-    }
-    render() {
-        return (
-            <div className="App">
-                <button className="switchBtn" onClick={this.switchMode}>
-                    Enable {this.state.buttonText}
-                </button>
-                <div className="resume">
-                    <div className="cv-header">
-                        <NameInfo />
-                        <PersonalInfo />
+    };
+
+    return (
+        <div className="App">
+            <button className="switchBtn" onClick={switchMode}>
+                Enable {buttonText}
+            </button>
+            <div className="resume">
+                <div className="cv-header">
+                    <NameInfo />
+                    <PersonalInfo />
+                </div>
+                <div className="cv-content">
+                    <div className="cv-work-experience">
+                        <ExperienceList
+                            removeExp={removeExp}
+                            experiences={experiences}
+                            preview={preview}
+                        />
+                        <AddExperience
+                            addExp={addExperience}
+                            preview={preview}
+                        />
                     </div>
-                    <div className="cv-content">
-                        <div className="cv-work-experience">
-                            <ExperienceList
-                                removeExp={this.removeExp}
-                                experiences={this.state.experiences}
-                                preview={this.state.preview}
-                            />
-                            <AddExperience
-                                addExp={this.addExperience}
-                                preview={this.state.preview}
-                            />
-                        </div>
-                        <div className="cv-education">
-                            <EducationList
-                                removeEdu={this.removeEdu}
-                                educations={this.state.education}
-                                preview={this.state.preview}
-                            />
-                            <AddEducation
-                                addEdu={this.addEducation}
-                                preview={this.state.preview}
-                            />
-                        </div>
+                    <div className="cv-education">
+                        <EducationList
+                            removeEdu={removeEdu}
+                            educations={education}
+                            preview={preview}
+                        />
+                        <AddEducation addEdu={addEducation} preview={preview} />
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
