c5e22f29f206638eb051aa5a90fe66ee09b953d0
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-27 10:34:09 +0800

    converted experiences to functional components

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -1,53 +1,44 @@
-import { Component } from 'react';
 import TextField from '../TextField/TextField';
 import './WorkExperience.css';
-class WorkExperience extends Component {
-    constructor(props) {
-        super(props);
-        this.removeExperience = this.removeExperience.bind(this);
-    }
 
-    removeExperience() {
-        this.props.removeExp(this.props.work.id);
-    }
+const WorkExperience = (props) => {
+    const removeExperience = () => {
+        props.removeExp(this.props.work.id);
+    };
 
-    render() {
-        const { work, preview } = this.props;
-        const { companyName, role, from, to, description } = work;
-        if (!preview) {
-            return (
-                <div className="work-experience hover">
-                    <div className="work-left">
-                        <TextField text={companyName} id="companyName" />
-                        <TextField text={`${from} - ${to}`} id="fromTo" />
-                    </div>
-                    <div className="work-right">
-                        <TextField id="companyRole" text={role} />
-                        <TextField id="job-description" text={description} />
-                    </div>
-                    <button
-                        className="removeExperience"
-                        onClick={this.removeExperience}
-                    >
-                        X
-                    </button>
+    const { work, preview } = props;
+    const { companyName, role, from, to, description } = work;
+
+    if (!preview) {
+        return (
+            <div className="work-experience hover">
+                <div className="work-left">
+                    <TextField text={companyName} id="companyName" />
+                    <TextField text={`${from} - ${to}`} id="fromTo" />
                 </div>
-            );
-        } else {
-            return (
-                <div className="work-experience hover">
-                    <div className="work-left">
-                        <TextField text={companyName} id="companyName" />
-                        <TextField text={`${from} - ${to}`} id="fromTo" />
-                    </div>
-                    <div className="work-right">
-                        <TextField id="companyRole" text={role} />
-                        <TextField id="job-description" text={description} />
-                    </div>
+                <div className="work-right">
+                    <TextField id="companyRole" text={role} />
+                    <TextField id="job-description" text={description} />
                 </div>
-            );
-        }
+                <button className="removeExperience" onClick={removeExperience}>
+                    X
+                </button>
+            </div>
+        );
+    } else {
+        return (
+            <div className="work-experience hover">
+                <div className="work-left">
+                    <TextField text={companyName} id="companyName" />
+                    <TextField text={`${from} - ${to}`} id="fromTo" />
+                </div>
+                <div className="work-right">
+                    <TextField id="companyRole" text={role} />
+                    <TextField id="job-description" text={description} />
+                </div>
+            </div>
+        );
     }
-}
+};
 
 export default WorkExperience;
